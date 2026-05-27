3e34178d63d6a394eee1c009425bc9d45970ab0d
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-05-27 16:19:59 +0100

    feat: add StartupMessage and Query parsers

--- messages.go
+++ messages.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"fmt"
 )
 
 // Message type bytes
@@ -295,4 +296,58 @@ func data_row(values [][]byte) ([]byte, error) {
 		}
 	}
 	return buf.Bytes(), nil
+}
+
+func parse_startup(data []byte) (string, string, int, error) {
+	if len(data) < 8 {
+		return "", "", 0, fmt.Errorf("message too short")
+	}
+	length := int(binary.BigEndian.Uint32(data[0:4]))
+	if len(data) < length {
+		return "", "", 0, fmt.Errorf("message incomplete")
+	}
+	protocol_version := binary.BigEndian.Uint32(data[4:8])
+	if protocol_version != 196608 {
+		return "", "", 0, fmt.Errorf("unsupported protocol version")
+	}
+
+	var user, database string
+	params := data[8:length]
+	i := 0
+	for i < len(params) && params[i] != 0 {
+		key_end := i + bytes.IndexByte(params[i:], 0)
+		key := string(params[i:key_end])
+		i = key_end + 1
+
+		val_end := i + bytes.IndexByte(params[i:], 0)
+		value := string(params[i:val_end])
+		i = val_end + 1
+
+		switch key {
+		case "user":
+			user = value
+		case "database":
+			database = value
+		}
+	}
+
+	return user, database, length, nil
+}
+
+func parse_query(data []byte) (string, error) {
+	if len(data) < 5 {
+		return "", fmt.Errorf("message too short")
+	}
+	if data[0] != 'Q' {
+		return "", fmt.Errorf("wrong message type")
+	}
+	length := int(binary.BigEndian.Uint32(data[1:5]))
+	if len(data) < 1+length {
+		return "", fmt.Errorf("message incomplete")
+	}
+	payload := data[5 : 1+length]
+	if len(payload) == 0 || payload[len(payload)-1] != 0 {
+		return "", fmt.Errorf("invalid format")
+	}
+	return string(payload[:len(payload)-1]), nil
 }
