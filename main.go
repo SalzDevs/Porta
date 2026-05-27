53ee92015e5e939914e52967f9ca6c3243aba494
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-05-27 23:04:54 +0100

    feat: add env var configuration (PORTA_LISTEN, PORTA_UPSTREAM, PORTA_POOL_SIZE)

--- main.go
+++ main.go
@@ -3,24 +3,42 @@ package main
 import (
 	"log"
 	"net"
+	"os"
+	"strconv"
 )
 
 func main() {
-	addr := "127.0.0.1:6432"
-	listener, err := net.Listen("tcp", addr)
+	listen := os.Getenv("PORTA_LISTEN")
+	if listen == "" {
+		listen = "127.0.0.1:6432"
+	}
+
+	upstream := os.Getenv("PORTA_UPSTREAM")
+	if upstream == "" {
+		upstream = "127.0.0.1:5432"
+	}
+
+	poolSize := 20
+	if s := os.Getenv("PORTA_POOL_SIZE"); s != "" {
+		if n, err := strconv.Atoi(s); err == nil && n > 0 {
+			poolSize = n
+		}
+	}
+
+	listener, err := net.Listen("tcp", listen)
 	if err != nil {
 		log.Fatalf("listen: %v", err)
 	}
 	defer listener.Close()
-	log.Printf("porta listening on %s", addr)
+	log.Printf("porta listening on %s", listen)
 
-	pool := NewPool(20)
+	pool := NewPool(poolSize)
 	for {
 		client, err := listener.Accept()
 		if err != nil {
 			log.Printf("accept: %v", err)
 			continue
 		}
-		go handleProxy(client, pool, "127.0.0.1:5432")
+		go handleProxy(client, pool, upstream)
 	}
 }
